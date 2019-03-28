d57208068863d2453df1ef3456c8e5b3d1572ea2
decinho13 <47136849+decinho13@users.noreply.github.com>
2019-03-28 09:15:40 +0100

    Update jupyter_notebook_config.py

--- base/jupyter_notebook_config.py
+++ base/jupyter_notebook_config.py
@@ -11,6 +11,7 @@ c = get_config()
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
+c.NotebookApp.password='YdRgN#kP6F'
 
 # https://github.com/jupyter/notebook/issues/3130
 c.FileContentsManager.delete_to_trash = False
